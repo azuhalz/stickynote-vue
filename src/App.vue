295414110e9f75bef213b80ba88351c20822473c
Ahmad Zuhal Zhafran <90975889+azuhalz@users.noreply.github.com>
2026-04-22 17:06:24 +0700

    feat: hide and show form

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const showForm = ref(false);
+</script>
 
 <template>
   <main>
     <div class="container">
       <header>
         <h1 class="header-title">Memo</h1>
-        <button class="header-button">+</button>
+        <button class="header-button" @click="showForm = true">+</button>
       </header>
 
       <div class="card-container">
@@ -38,6 +42,14 @@
           </p>
           <p class="card-date">12/12/2026</p>
         </div>
+      </div>
+    </div>
+
+    <div v-if="showForm" class="form-overlay">
+      <div class="form-modal">
+        <button class="form-close-btn">x</button>
+        <textarea name="memo" id="memo" cols="30" rows="10"></textarea>
+        <button class="form-save-btn">Save</button>
       </div>
     </div>
   </main>
@@ -95,5 +107,52 @@ header {
   display: flex;
   gap: 20px;
   flex-wrap: wrap;
+}
+
+.form-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.77);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 10;
+}
+
+.form-modal {
+  width: 420px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 30px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+
+.form-save-btn {
+  padding: 10px 20px;
+  font-size: 20px;
+  width: 100%;
+  background-color: #495a7d;
+  color: white;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+  margin-top: 15px;
+}
+
+.form-close-btn {
+  position: absolute;
+  top: 2px;
+  right: 3px;
+  width: 30px;
+  height: 30px;
+  background-color: transparent;
+  border: none;
+  font-size: 25px;
+  cursor: pointer;
 }
 </style>
